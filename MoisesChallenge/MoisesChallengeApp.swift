a48d11fad24f8fed41df5d002f61de7d3ce13a16
ChavesYuri <33982282+ChavesYuri@users.noreply.github.com>
2026-05-24 10:57:52 -0300

    Initial Commit

---
+++ MoisesChallenge/MoisesChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoisesChallengeApp.swift
+//  MoisesChallenge
+//
+//  Created by Yuri Chaves on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MoisesChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoisesChallengeTests/MoisesChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoisesChallengeTests.swift
+//  MoisesChallengeTests
+//
+//  Created by Yuri Chaves on 24/05/26.
+//
+
+import Testing
+@testable import MoisesChallenge
+
+struct MoisesChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
